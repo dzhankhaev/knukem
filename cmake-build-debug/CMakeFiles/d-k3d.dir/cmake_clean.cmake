f6e3dd5cc275e649c32af545a952dd404017122f
dzhankhaev <dzhankhaev.d@gmail.com>
2020-10-30 23:43:37 +0300

    2

---
+++ cmake-build-debug/CMakeFiles/d-k3d.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/d-k3d.dir/src/cut_wall.c.o"
+  "CMakeFiles/d-k3d.dir/src/game_loop.c.o"
+  "CMakeFiles/d-k3d.dir/src/init_engine.c.o"
+  "CMakeFiles/d-k3d.dir/src/keys_manager.c.o"
+  "CMakeFiles/d-k3d.dir/src/lighting.c.o"
+  "CMakeFiles/d-k3d.dir/src/main.c.o"
+  "CMakeFiles/d-k3d.dir/src/manage_data.c.o"
+  "CMakeFiles/d-k3d.dir/src/math_utilits/determine_intersection.c.o"
+  "CMakeFiles/d-k3d.dir/src/math_utilits/intersection_point.c.o"
+  "CMakeFiles/d-k3d.dir/src/math_utilits/mathf.c.o"
+  "CMakeFiles/d-k3d.dir/src/math_utilits/perspective_transform.c.o"
+  "CMakeFiles/d-k3d.dir/src/math_utilits/rotates.c.o"
+  "CMakeFiles/d-k3d.dir/src/moving/fall.c.o"
+  "CMakeFiles/d-k3d.dir/src/moving/move.c.o"
+  "CMakeFiles/d-k3d.dir/src/render/line/render_line.c.o"
+  "CMakeFiles/d-k3d.dir/src/render/line/render_vline.c.o"
+  "CMakeFiles/d-k3d.dir/src/render/minimap.c.o"
+  "CMakeFiles/d-k3d.dir/src/render/render_wall.c.o"
+  "CMakeFiles/d-k3d.dir/src/run_queue.c.o"
+  "CMakeFiles/d-k3d.dir/src/transform_wall.c.o"
+  "d-k3d"
+  "d-k3d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/d-k3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
