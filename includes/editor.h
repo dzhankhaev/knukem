72302f909db24124a7d85927635ef300f9b14256
Ivanov Aleksandr <bioid.iad@gmail.com>
2021-02-08 23:24:19 +0300

    norm header

--- includes/editor.h
+++ includes/editor.h
@@ -60,200 +60,197 @@
 # define GRAVITY 7
 # define AMMO 8
 
-typedef struct			s_fline
+typedef struct		s_fline
 {
-	float				x0;
-	float				x1;
-	float				y0;
-	float				y1;
-	Uint32				color;
-}						t_fline;
+	float			x0;
+	float			x1;
+	float			y0;
+	float			y1;
+	Uint32			color;
+}					t_fline;
 
-typedef struct	    s_sdl
+typedef struct		s_sdl
 {
-    SDL_Window	    *window;
+    SDL_Window		*window;
     SDL_Surface		*screen;
-}				    t_sdl;
+}					t_sdl;
 
-typedef struct      s_xyint
+typedef struct		s_xyint
 {
-    int           x;
-    int           y;
-}                   t_xyint;
+    int				x;
+    int				y;
+}					t_xyint;
 
-typedef struct      s_xy
+typedef struct		s_xy
 {
-    float           x;
-    float           y;
-}                   t_xy;
+    float			x;
+    float			y;
+}					t_xy;
 
-typedef struct	s_xyz
+typedef struct		s_xyz
 {
-    float		x;
-    float		y;
-    float		z;
-}				t_xyz;
+    float			x;
+    float			y;
+    float			z;
+}					t_xyz;
 
-typedef struct			s_graf
+typedef struct		s_graf
 {
-	int					g_num;		//	количество граффити в секторе
-	float				*z;			//	высота
-	t_fline				*coord;		//	вектор
-	int					*wall;		//	номер стены сектора
-	int					*u;
-}						t_graf;
+	int				g_num;
+	float			*z;
+	t_fline			*coord;
+	int				*wall;
+	int				*u;
+}					t_graf;
 
 typedef struct		s_sect
 {
     float			floor;
     float			ceil;
-    t_xy			*vertex;		//	Координаты всех вершин данного сектора, причем первая координаты дублируется в конце
-    int				*neighbors;		//	Номера всех соседей данного сектора
-    unsigned		npoints;		//	Количество соседей
-    int				door;			//	-1 не является дверью. Остальное показывает насколько дверь закрыта
-    float			oldf;			//	уровень пола, если сектор является дверью
+    t_xy			*vertex;
+    int				*neighbors;
+    unsigned		npoints;
+    int				door;
+    float			oldf;
     int				txf;
     int				txc;
     int				txw0;
     int				txw1;
     t_graf			graf;
-
 }					t_sect;
 
-
-typedef struct      s_button
+typedef struct		s_button
 {
-    
-    SDL_Rect        dstrect;
-    char			*title;
-    SDL_Color		color;
-    int             state;
-    SDL_Surface		*texture;
-}                   t_button;
+	SDL_Rect		dstrect;
+	char			*title;
+	SDL_Color		color;
+	int				state;
+	SDL_Surface		*texture;
+}					t_button;
 
-typedef struct  s_settings
+typedef struct		s_settings
 {
-    int         inventory;
-    int         speed;
-    int         gravity;
-    int         ammo;
-}               t_settings;
+	int				inventory;
+	int				speed;
+	int				gravity;
+	int				ammo;
+}					t_settings;
 
 
-typedef	struct	s_player
+typedef	struct		s_player
 {
-    t_xyz		where;
-    t_xyz		velocity;		//	вектор скорости движения
-    SDL_Surface	*picture;
-    int			picked;
-    float		angle;			//	угол поворота
-    float		anglesin;		//	синус угла поворота
-    float		anglecos;		//	косинус угла поворота
-    float		vangle;			//	угол вертикального поворота
-    int 		sector;			//	текущий сектор
-    int			wsad[4];		//	ключи передвижения. 1 - движение, 0 - его отсутстие
-    int			falling;		//	1 - игрок падает, 0 - не падает. Игрок может падать вверх. Если 1, будет применено ускорение
-    int			flying;			//	флаг = 1 только если игрок летит вверх или вниз. ФЛАГ НЕ ОПРЕДЕЛЯЕТ НАХОДИТСЯ ИГРОК В РЕЖИМЕ ПОЛЕТА ИЛИ НЕТ
-    int			ground;			//	грок стоит на земле
-    int			stand;
-    float		eyeheight;		//	текущая высота камеры
-    float		speed;			//	текущая максимальная скорость
-    float		move_speed;		//	Максимальная скорость
-    int			deep_sh;		//	затенение
-    SDL_Event	event;
-    int			game_mode;		//	1 создатель, 0 игрок
-    t_settings  settings;
-    int			cur_inv;		//	Сколько мест в инвентаре занято
-    int			cur_am;			//	Сколько патронов имеется
-    int			shot;			//	0 ничего, 1 выстрел
-}				t_player;
+    t_xyz			where;
+    t_xyz			velocity;
+    SDL_Surface		*picture;
+    int				picked;
+    float			angle;
+    float			anglesin;
+    float			anglecos;
+    float			vangle;
+    int 			sector;
+    int				wsad[4];
+    int				falling;
+    int				flying;
+    int				ground;
+    int				stand;
+    float			eyeheight;
+    float			speed;
+    float			move_speed;
+    int				deep_sh;
+    SDL_Event		event;
+    int				game_mode;
+    t_settings		settings;
+    int				cur_inv;
+    int				cur_am;
+    int				shot;
+}					t_player;
 
-typedef	struct s_labels
+typedef	struct		s_labels
 {
     SDL_Surface		*surf;
     SDL_Rect		dst;
-}				t_labels;
+}					t_labels;
 
-typedef struct      s_all
+typedef struct		s_all
 {
-    t_player        player;// переменная игрока.
+    t_player		player;
     int				swap_num;
-    int             fin_sect;
-    t_xy			delta; // дельта области редактирования
-    t_xy			d; // дельта сдвига карты
-    t_sect          *sectors;//массив с данными о секторах
+    int				fin_sect;
+    t_xy			delta;
+    t_xy			d;
+    t_sect			*sectors;
     t_sect			temp;
-    int				num_sectors;//количество секторов
-    int 			step;//шаг (масштаб)
+    int				num_sectors;
+    int 			step;
     TTF_Font		*font;
     t_labels		labels[9];
-    t_xy			draw_floors;//уровни отрисовки этажей
-    t_xy			set_floors;//установка высоты нового сектора
-    t_xyint			point;//координаты ближайшей точки курсора
-    t_xyz			mouse;//координаты мыши
-    t_xyz			mapsize;//размер карты в исходной СИ
+    t_xy			draw_floors;
+    t_xy			set_floors;
+    t_xyint			point;
+    t_xyz			mouse;
+    t_xyz			mapsize;
     t_xy			min_coord;
     t_xy			max_coord;
-    SDL_Rect        area;//область редактирования
-    SDL_Surface     *texture;//текстура-подложка
+    SDL_Rect		area;
+    SDL_Surface		*texture;
     int				threed;
-    t_button        buttons[BUTTONS];//кнопки
-    t_sdl           sdl;//
-    char			*name;
-    char            *src_dir;
-    char            *src_map;
-}                   t_all;
+    t_button		buttons[BUTTONS];
+    t_sdl			sdl;    
+	char			*name;
+    char			*src_dir;
+    char			*src_map;
+}					t_all;
 
-void	            init_all(t_all *all); // инициализируем модули
-void                error_and_close(const char *file, const char *function); // аварийное завершение программы
-void                on_event(t_all *all, SDL_Event *event); //обработка событий
+void				init_all(t_all *all);
+void				error_and_close(const char *file, const char *function);
+void				on_event(t_all *all, SDL_Event *event);
 void				map_click(t_all *all, SDL_MouseButtonEvent *event);
-int					load_map(char *name, t_all *all); // загрузка карты
-int                 load_texture(t_all *all);// звгрузка текстур
+int					load_map(char *name, t_all *all);
+int					load_texture(t_all *all);
 int					load_buttons(t_button *btn);
-int                 check_sect_intersect(t_all *all, int num, t_xy one, t_xy two);
-void                check_vert(t_all *all, int *x, int *y, t_sect *temp);
-void                draw_all(t_all *all, t_sdl *sdl, t_button *btn);//отрисовка
-void	            draw_digits(t_sdl *sdl, t_all *all, int x, int y);
-void	            draw_setting_digits(t_all *all, t_settings *settings);
+int					check_sect_intersect(t_all *all, int num, t_xy one, t_xy two);
+void				check_vert(t_all *all, int *x, int *y, t_sect *temp);
+void				draw_all(t_all *all, t_sdl *sdl, t_button *btn);
+void				draw_digits(t_sdl *sdl, t_all *all, int x, int y);
+void				draw_setting_digits(t_all *all, t_settings *settings);
 int					write_map(t_all *all);
-void				draw_temp(t_all *all, t_sdl *sdl, t_sect *temp, t_xy delta);//несохраненный сектор
-void                draw_player(t_all *all, t_sdl *sdl, t_player *player, t_xy *c);
+void				draw_temp(t_all *all, t_sdl *sdl, t_sect *temp, t_xy delta);
+void				draw_player(t_all *all, t_sdl *sdl, t_player *player, t_xy *c);
 void				draw_map(t_sect *sect, t_all *all);
 void				draw_grid(t_all *all, SDL_Rect *area, int step);
-void    			draw_texture(SDL_Surface *screen, SDL_Rect area, SDL_Surface *txt);
-void    			draw_fill_rect(t_all *all, SDL_Rect area, SDL_Color color);
+void				draw_texture(SDL_Surface *screen, SDL_Rect area, SDL_Surface *txt);
+void				draw_fill_rect(t_all *all, SDL_Rect area, SDL_Color color);
 void				draw_rect(t_all *all, SDL_Rect area, SDL_Color color, int border);
 void				draw_line(t_all *all, t_xy *start, t_xy *fin, SDL_Color color);
-void	            draw_label(t_all *all, char *name, SDL_Rect area, SDL_Color color);
+void				draw_label(t_all *all, char *name, SDL_Rect area, SDL_Color color);
 void				draw_circle(t_sdl *sdl, t_xy coord, int r, SDL_Color color);
 void				draw_ui(t_all *all, t_sdl *sdl, t_button *btn);
-void	            draw_sector(t_sect *sect, t_all *all, SDL_Color color, t_xy delta);
-void	            processing(t_all *all);
-int		            is_vector_equal(t_xy a0, t_xy a1, t_xy b0, t_xy b1);
-void    			new_sector(t_all *all, t_sect *temp, int x, int y);
-int                 get_neighbours(t_sect *sector, t_all 	*all, int n);
+void				draw_sector(t_sect *sect, t_all *all, SDL_Color color, t_xy delta);
+void				processing(t_all *all);
+int					is_vector_equal(t_xy a0, t_xy a1, t_xy b0, t_xy b1);
+void				new_sector(t_all *all, t_sect *temp, int x, int y);
+int					get_neighbours(t_sect *sector, t_all 	*all, int n);
 float				point_side1(float px, float py, t_xy vert, t_xy vert1);
 int					check_sector(t_sect *sect);
-int                 inside_sector(t_xyint point, t_sect *sector);
+int					inside_sector(t_xyint point, t_sect *sector);
 int					which_sector(t_all *all, t_sect *sectors, t_xyz where);
-int		            which_sector_p(t_all *all, t_sect *sectors, t_xyz where);
-int                 *which_sectors(t_all *all, t_sect *sectors, t_xyz where);
+int					which_sector_p(t_all *all, t_sect *sectors, t_xyz where);
+int					*which_sectors(t_all *all, t_sect *sectors, t_xyz where);
 void				remove_sector(t_all *all, t_sect *sectors);
 SDL_Surface			*get_text_surface(char *name, TTF_Font *font, SDL_Color color);
-void       			put_pxl(SDL_Surface *screen, SDL_Color col, int x, int y);
-t_xy                get_coords(t_all *all);
+void				put_pxl(SDL_Surface *screen, SDL_Color col, int x, int y);
+t_xy				get_coords(t_all *all);
 void				init_floors(t_sect *sectors, int num);
 void				normalize(t_sect *sectors, int num, t_all *all);
 int					**get_vertexes(t_all *all);
 int					get_order_num(t_xy coord, int **vert);
-int                 is_intersectred(t_xy v11, t_xy v12, t_xy v21, t_xy v22);
-int		            doors_close(t_all *all);
-void	            key_press(t_all *all);
-int                 print_message(t_all *all, SDL_Color color, char *text, int delay);
+int					is_intersectred(t_xy v11, t_xy v12, t_xy v21, t_xy v22);
+int					doors_close(t_all *all);
+void				key_press(t_all *all);
+int					print_message(t_all *all, SDL_Color color, char *text, int delay);
 void				new_map(char *name);
-void                closest_point(t_all *all, t_xyint point);
-void                write_sectors(t_all *all, int **vert, int fd);
+void				closest_point(t_all *all, t_xyint point);
+void				write_sectors(t_all *all, int **vert, int fd);
 int					mode_switch(t_all *all, int mode);
 void				sector0(int fd);
 void				sector1(int fd);
